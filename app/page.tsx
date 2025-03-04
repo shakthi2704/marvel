cd3cfccad5380f8d2c31f0b3d429126a063d34c6
Satheesh <shakthi2704@gmail.com>
2025-03-04 09:59:41 +0530

    meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold underline">Hello Marvel!</h1>
     </>
   )
 }
